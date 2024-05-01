7cf26bcb14eb2bd43d616dd8d8a60efcca39473c
ArunBharathan <arunbharathannj@gmail.com>
2024-05-01 11:51:26 +0530

    timestam project

--- index.js
+++ index.js
@@ -2,31 +2,48 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
+app.get("/api/:date?", (req, resp) => {
+  let date = req?.params?.date;
+  let unix = null,
+    utc = null;
+  // console.log(date, "Type of date =>", typeof date);
+  date = isNaN(Date.parse(date)) ? parseInt(date) : date;
 
+  console.log(date, "Type of date =>", typeof date);
+  try {
+    unix = new Date(date).getTime();
+    utc = new Date(date).toUTCString();
+    if (utc === "Invalid Date") {
+      throw new Error("Invalid Date");
+    }
+    resp.json({ unix, utc });
+  } catch (error) {
+    resp.json({ error: "Invalid Date" });
+  }
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
